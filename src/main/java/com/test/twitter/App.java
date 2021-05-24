33dfd8cd1a8e922b151187b4ee65144b161757ad
kelly-may <kellymay99@gmail.com>
2021-05-24 15:39:28 -0400

    Added Maven and Twitter4J API JAR

--- src/main/java/com/test/twitter/App.java
+++ src/main/java/com/test/twitter/App.java
@@ -4,7 +4,14 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
+import java.util.ArrayList;
 
+/**
+ * Twitter Search Program
+ * Description - Java Swing application that searches Twitter for a user's query and displays the results.
+ * Author - Kelly May
+ * Date Created - 5/24/2021
+ */
 public class App {
     private JButton btnEnter;
     private JPanel panelMain;
@@ -12,15 +19,28 @@ public class App {
     private JList list1;
     private JTextPane txtTitle;
     private JTextPane txtSubtitle;
+    private ArrayList<String> searchFields = new ArrayList<>();
 
+    /**
+     * App - holds listeners
+     */
     public App() {
         btnEnter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 //write code to show message
                 JOptionPane.showMessageDialog(null, "Hello World!");
+                createList();
             }
         });
+    }
+
+    /**
+     * createList - method to fill the list1 values with top 5 tweets.
+     */
+    public void createList(){
+        searchFields.add("Testing This");
+        list1.setListData(searchFields.toArray());
     }
 
     public static void main(String[] args){
